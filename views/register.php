3a5d8bb55ddacb44c3777c014e98f486bf830a59
Nikita <n.sokolskii@g.nsu.ru>
2020-12-09 15:36:48 +0700

    Minor fixes in post page

--- views/register.php
+++ views/register.php
@@ -6,26 +6,3 @@
     <?php echo $form->field($model, 'confirmPassword')->passwordField(); ?>
     <button type="submit" class="btn btn-primary">Submit</button>
 <?php echo \app\core\form\Form::end(); ?>
-<!-- <form action="" method="post">
-  <div class="form-group">
-    <label>Firstname</label>
-    <input type="text" name="firstname" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>Lastname</label>
-    <input type="text" name="lastname" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>E-mail</label>
-    <input type="text" name="email" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>Password</label>
-    <input type="password" name="password" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>Confirm password</label>
-    <input type="password" name="confirmPassword" class="form-control">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form> -->
